59729362796a1cdc7bdb85d34a84b9315cc84e97
Yan Postnov <y.postnov@helsi.me>
2020-12-05 14:37:03 +0200

    fix joi validation schema type; fix pachages.json dependecies

--- src/utils/validate/index.ts
+++ src/utils/validate/index.ts
@@ -1,7 +1,10 @@
-import Joi from "joi";
+import Joi, { SchemaMap } from "joi";
 import { ValidationSchema, validationSchema } from "./shemas";
 
-const validate = <Data>(schema: ValidationSchema, data: Data) => {
+const validate = <Data>(
+  schema: SchemaMap<ValidationSchema> | undefined,
+  data: Data
+) => {
   return Joi.object({ ...schema })
     .options({ abortEarly: false })
     .validate({ ...data });
